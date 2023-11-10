9c3de177f13016b6d3fe4e4c99522ba8fcb98c78
Davidekk <david.halcin00@gmail.com>
2023-11-10 20:26:36 +0100

    add route api

---
+++ app/api/nfc/route.js
@@ -0,0 +1,13 @@
+"use server";
+
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  try {
+    const body = await request.json();
+    console.log(body);
+    return NextResponse.json({ data: body.uid }, { status: 200 });
+  } catch (error) {
+    NextResponse.json({ error: "Internal Server Error" });
+  }
+}
